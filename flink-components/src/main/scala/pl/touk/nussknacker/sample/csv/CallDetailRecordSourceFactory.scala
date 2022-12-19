e99820a3f8615b75e1a633c745a0501a9c5a7d51
Maciej Próchniak <mpr@touk.pl>
2022-12-19 14:55:40 +0100

    Upgrade to NU 1.7 (#12)
    * Upgrade to NU 1.7
    
    * release

--- flink-components/src/main/scala/pl/touk/nussknacker/sample/csv/CallDetailRecordSourceFactory.scala
+++ flink-components/src/main/scala/pl/touk/nussknacker/sample/csv/CallDetailRecordSourceFactory.scala
@@ -1,14 +1,20 @@
 package pl.touk.nussknacker.sample.csv
 
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import pl.touk.nussknacker.engine.api.process.SourceFactory
+import pl.touk.nussknacker.engine.flink.typeinformation.ConcreteCaseClassTypeInfo
 
 import java.io.File
 import java.time.Duration
 
 object CallDetailRecordSourceFactory {
   def prepare(filesDir: String, separator: Char): SourceFactory = {
-    import org.apache.flink.api.scala.createTypeInformation
-
+    implicit val typeInfo: ConcreteCaseClassTypeInfo[CallDetailRecord] = ConcreteCaseClassTypeInfo[CallDetailRecord](
+      "phoneNumberA" -> TypeInformation.of(classOf[String]),
+      "phoneNumberB" -> TypeInformation.of(classOf[String]),
+      "callDuration" -> TypeInformation.of(classOf[Duration]),
+      "callStartTime" -> TypeInformation.of(classOf[Long])
+    )
     new SpecificRecordCsvSourceFactory[CallDetailRecord](new File(filesDir), separator, CallDetailRecord.fromFields, _.callStartTime)
   }
 }
